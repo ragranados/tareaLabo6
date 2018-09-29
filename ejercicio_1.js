88d212efb1cc34e6bc033387a79c124a54627b56
Raul <00138816@uca.edu.sv>
2018-09-28 23:36:11 -0600

    finalizado

--- ejercicio_1.js
+++ ejercicio_1.js
@@ -2,7 +2,7 @@ productos = [];
 ventas = [];
 numProd = 0;
 
-var agregarProducto = function(descripcion, tipo, precioCompra, precioVenta, stock){
+var agregarProducto = function (descripcion, tipo, precioCompra, precioVenta, stock) {
     numProd = numProd + 1;
     var producto = {};
 
@@ -16,33 +16,33 @@ var agregarProducto = function(descripcion, tipo, precioCompra, precioVenta, sto
     productos.push(producto);
 }
 
-var modificarStock = function(idProducto, nuevoStock){
-    productos.forEach((element,index) => {
-        if(element.codigo==idProducto){
-            productos[index].stock=nuevoStock;
+var modificarStock = function (idProducto, nuevoStock) {
+    productos.forEach((element, index) => {
+        if (element.codigo == idProducto) {
+            productos[index].stock = nuevoStock;
             return 1;
         }
     });
     return 0;
 }
 
-var crearVenta = function(codigo,cantidad,precio){
+var crearVenta = function (codigo, cantidad, precio) {
     var venta = {};
 
     venta.codigo = codigo;
-    venta.total = cantidad*precio;
+    venta.total = cantidad * precio;
     ventas.push(venta);
 }
 
-var registrarVenta = function(idProducto,cantidad){
-    productos.forEach((element,index) => {
-        if(element.codigo==idProducto){
-            if((element.stock-cantidad)<0){
+var registrarVenta = function (idProducto, cantidad) {
+    productos.forEach((element, index) => {
+        if (element.codigo == idProducto) {
+            if ((element.stock - cantidad) < 0) {
                 console.log("no hay suficiente producto");
                 return 1;
-            }else{
+            } else {
                 productos[index].stock -= cantidad;
-                crearVenta(element.codigo,cantidad,element.precioVenta);
+                crearVenta(element.codigo, cantidad, element.precioVenta);
                 return 1;
             }
         }
@@ -50,21 +50,21 @@ var registrarVenta = function(idProducto,cantidad){
     return 0;
 }
 
-var promedioVentas = function(){
+var promedioVentas = function () {
     let suma = 0;
 
-    for(let e of ventas){
+    for (let e of ventas) {
         suma += e.total;
     }
 
-    return (suma/ventas.length);
+    return (suma / ventas.length);
 }
 
-var productosAgotados = function(){
+var productosAgotados = function () {
     aux = "";
-    productos.forEach((element,index) => {
-        if(element.stock==0){
-            aux = aux + element.descripcion +" | ";
+    productos.forEach((element, index) => {
+        if (element.stock == 0) {
+            aux = aux + element.descripcion + " | ";
         }
     });
 
@@ -72,46 +72,46 @@ var productosAgotados = function(){
     return aux;
 }
 
-var menu = function(){
+var menu = function () {
     var adentro = true;
     var op;
 
-    while(adentro){
+    while (adentro) {
         op = parseInt(prompt("1. Agregar Producto\n2. Modificar Stock\n3. Registrar Venta\n4. Mostrar promedio\n5. Agotados\n6. Salir"));
-        if(op == 1){
+        if (op == 1) {
             let descripcion = prompt("Descripcion: ");
             let tipo = prompt("Tipo: ");
             let precioCompra = parseInt(prompt("Ingrese precio de compra: "));
             let precioVenta = parseInt(prompt("Ingrese precio de venta: "));
             let stock = parseInt(prompt("Ingrese stock: "));
 
-            agregarProducto(descripcion,tipo,precioCompra,precioVenta,stock);
+            agregarProducto(descripcion, tipo, precioCompra, precioVenta, stock);
 
-        } else if(op == 2){
+        } else if (op == 2) {
             var prods = "";
-            for(let prod of productos){
+            for (let prod of productos) {
                 prods += prod.codigo + ". " + prod.descripcion + "\n";
             }
 
             let codigo = prompt(prods + "ingrese codigo: ");
             let nuevoS = prompt("Ingrese nuevo stock: ");
 
-            modificarStock(codigo,nuevoS);
-        } else if(op == 3){
+            modificarStock(codigo, nuevoS);
+        } else if (op == 3) {
             var prods = "";
-            for(let prod of productos){
+            for (let prod of productos) {
                 prods += prod.codigo + ". " + prod.descripcion + "\n";
             }
 
             let codigo = prompt(prods + "ingrese codigo: ");
             let cantidad = prompt("Ingrese cantidad vendida: ");
 
-            registrarVenta(codigo,cantidad);
-        }else if(op == 4){
+            registrarVenta(codigo, cantidad);
+        } else if (op == 4) {
             promedioVentas();
-        }else if(op == 5){
+        } else if (op == 5) {
             productosAgotados();
-        }else if(op == 6){
+        } else if (op == 6) {
             adentro = false;
         }
     }
---
+++ ejercicio_2.js
@@ -0,0 +1,29 @@
+class Conversor {
+    convertir(medida, uniOriginal, uniASer, tipo) {
+
+        if (tipo == "L") {
+
+            if (uniOriginal == "m" && uniASer == "cm") {
+                medida = medida * 100;
+            } else if(uniOriginal == "km" && uniASer == "m") {
+                medida = medida *1000;
+            } else if(uniOriginal == "ft" && uniASer == "m") {
+                medida = medida *0.3048;
+            }
+
+        } else if (tipo == "T") {
+            if(uniOriginal == "C" && uniASer == "F"){
+                medida = ((9/2)*medida)+32;
+            }else if(uniOriginal == "K" && uniASer == "C"){
+                medida = medida - 273.15;
+                medida = (medida*1.8)+32;
+            }else if(uniOriginal == "K" && uniASer == "C"){
+                medida = medida - 273.15;
+            }
+        }
+
+        return medida;
+    }
+}
+
+var Conversor1 = new Conversor();
