287a17623b83a253e5751e1ffdbc50a89060a63e
Raul <00138816@uca.edu.sv>
2018-09-28 22:11:14 -0600

    agregando menu

--- ejercicio_1.js
+++ ejercicio_1.js
@@ -1,37 +1,116 @@
 productos = [];
 ventas = [];
+numProd = 0;
 
-var agregarProducto = function(codigo, descripcion, tipo, precioCompra, precioVenta, stock){
-    producto = {};
+var agregarProducto = function(descripcion, tipo, precioCompra, precioVenta, stock){
+    numProd = numProd + 1;
+    var producto = {};
 
-    producto.codigo = codigo;
-    producto.descrpcion = descripcion;
+    producto.codigo = numProd;
+    producto.descripcion = descripcion;
     producto.tipo = tipo;
     producto.precioCompra = precioCompra;
     producto.precioVenta = precioVenta;
     producto.stock = stock;
+
+    productos.push(producto);
 }
 
 var modificarStock = function(idProducto, nuevoStock){
     productos.forEach((element,index) => {
-        if(element.codigo===idProducto){
-            producto[index].stock=nuevoStock;
-            return "modificacion hecha";
+        if(element.codigo==idProducto){
+            productos[index].stock=nuevoStock;
+            return 1;
         }
     });
-    return "no existe el producto";     
+    return 0;
+}
+
+var crearVenta = function(codigo,cantidad,precio){
+    var venta = {};
+
+    venta.codigo = codigo;
+    venta.total = cantidad*precio;
+    ventas.push(venta);
 }
 
 var registrarVenta = function(idProducto,cantidad){
     productos.forEach((element,index) => {
-        if(element.codigo===idProducto){
-            if((element.stock-idProducto)<0){
-                return "no se puede hacer";
+        if(element.codigo==idProducto){
+            if((element.stock-cantidad)<0){
+                console.log("no hay suficiente producto");
+                return 1;
             }else{
                 productos[index].stock -= cantidad;
+                crearVenta(element.codigo,cantidad,element.precioVenta);
+                return 1;
             }
         }
     });
+    return 0;
+}
 
-    return "correctamente";
+var promedioVentas = function(){
+    let suma = 0;
+
+    for(let e of ventas){
+        suma += e.total;
+    }
+
+    return (suma/ventas.length);
+}
+
+var productosAgotados = function(){
+    aux = "";
+    productos.forEach((element,index) => {
+        if(element.stock==0){
+            aux = aux + element.tipo +" | ";
+        }
+    });
+
+    return aux;
+}
+
+var menu = function(){
+    var adentro = true;
+    var op;
+
+    while(adentro){
+        op = parseInt(prompt("1. Agregar Producto\n2. Modificar Stock\n3. Registrar Venta\n4. Mostrar promedio\n5. Agotados\n6. Salir"));
+        if(op == 1){
+            let descripcion = prompt("Descripcion: ");
+            let tipo = prompt("Tipo: ");
+            let precioCompra = parseInt(prompt("Ingrese precio de compra: "));
+            let precioVenta = parseInt(prompt("Ingrese precio de venta: "));
+            let stock = parseInt(prompt("Ingrese stock: "));
+
+            agregarProducto(descripcion,tipo,precioCompra,precioVenta,stock);
+
+        } else if(op == 2){
+            var prods = "";
+            for(let prod of productos){
+                prods += prod.codigo + ". " + prod.descripcion + "\n";
+            }
+
+            let codigo = prompt(prods + "ingrese codigo: ");
+            let nuevoS = prompt("Ingrese nuevo stock: ");
+
+            modificarStock(codigo,nuevoS);
+        } else if(op == 3){
+            var prods = "";
+            for(let prod of productos){
+                prods += prod.codigo + ". " + prod.descripcion + "\n";
+            }
+
+            let codigo = prompt(prods + "ingrese codigo: ");
+            let cantidad = promt("Ingrese cantidad vendida: ");
+
+            registrarVenta(codigo,cantidad);
+        }else if(op == 4){
+            
+        } 
+        else if(op == 6){
+            adentro = false;
+        }
+    }
 }
